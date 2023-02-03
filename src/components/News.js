bd10c3643e04c5325c8fb89b2b8be1803f8c58e7
vaibhav <vn03485@gmail.com>
2023-02-04 00:06:24 +0530

    this is my second commit and in this i have removed a space

--- src/components/News.js
+++ src/components/News.js
@@ -42,7 +42,6 @@ const News = (props) => {
 
 
     const fetchMoreData = async () => {
-
         let urls = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apikey=${props.apiKey}&page=${page + 1}&pageSize=${props.pageSize}`
         setPage(page + 1);
         setLoading(true)
